852175e96782bbcfc34a46d9f0d14e60f389c01b
Taddaeus Nauheimer <taddaeus.nauheimer@mni.thm.de>
2021-05-26 03:58:45 +0200

    Added list of documents for correction

--- src/interfaces/Document.py
+++ src/interfaces/Document.py
@@ -12,6 +12,7 @@ class Document:
     img: Image = field(compare=False)
     tasks: List[Task] = field(default_factory=list, compare=True)
     img_modified: Image = field(default=None, compare=False, init=False)
+    documents_to_check : List[Image] = field(default=None, compare=False, init=False)
 
     def __post_init__(self):
         self.img_modified=self.img
