263f24358837663725bc5515a6846e0f8d37a3d8
Bautrelle Djuikom Fotso <bautrelle.djuikom.fotso@mni.thm.de>
2021-08-02 22:43:54 +0200

    test_new_tesseract_model

--- src/backend/build_models/build_mnist_model.py
+++ src/backend/build_models/build_mnist_model.py
@@ -1,96 +1,68 @@
 #!/usr/bin/python3
 
-import os
-
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-import tensorflow as tf
-import numpy as np
-#import pandas as pd
-from random import randint
-import matplotlib.pyplot as plt
+import os, tensorflow as tf
 keras = tf.keras
-from keras.models import Sequential, load_model
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.utils import np_utils
+
+''' 
+    Loads mnist train and test dataset from the keras.datasets library.
+    The mapp is an array of 10 elements representing the label of each class(0 to 9).
+    Returns each tuple of trainings an test set.
+'''
+def load_mnist():
+
+    (train_data, train_label), (test_data, test_label) = keras.datasets.mnist.load_data()
+
+    #change array type from uint8 to float32 to allow normalization
+    train_data = train_data.astype('float32')
+    test_data = test_data.astype('float32')
+
+    mapp = list(range(10))
+
+    # normalize the datas by scaling the pixel intensities 
+    # of the images from [0, 255] down to [0, 1]
+    train_data /= 255.0
+    test_data /= 255.0
+
+    return (train_data, train_label), (test_data, test_label), mapp
+
+''' 
+    Builds a deep neural network with 2 hidden layers and train it on the mnist training set.
+    validates the model on the test set and save the results(validation loss and validation accuracy).
+'''
+def train_mnist():
+
+    (train_data, train_label), (test_data, test_label), mapp = load_mnist()
+
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),     # input layer
+
+        keras.layers.Dense(512, activation="relu"),     # hidden layer
+
+        keras.layers.Dense(512, activation="relu"),     # hidden layer
+
+        keras.layers.Dense(10,  activation="softmax")   # output layer
+    ])
+
+    #compile the built deep neural network.
+    model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+    # train network with trainings set and validates on test set
+    history = model.fit(train_data, train_label, epochs=10, validation_data=(test_data, test_label))
+
+    # give the results of the network evaluation on the test set
+    test_loss, test_acc = model.evaluate(test_data,  test_label)
+
+    #saving the model
+    save_dir = "results"
+    model_name = "trained_mnist"
+    model_path = os.path.join(save_dir, model_name)
+    model.save(model_path)
+    print('saved trained model at %s ', model_path)
+
+    # predict each image in the test set using the built model
+    prediction = model.predict(test_data)
 
 
-(train_data, train_label), (test_data, test_label) = keras.datasets.mnist.load_data()
-
-#reshape images of mnist dataset train and set(building the input vector from 28x28)
-train_data = train_data.reshape(60000, 28, 28)
-test_data = test_data.reshape(10000, 28, 28)
-train_data = train_data.astype('float32')
-test_data = test_data.astype('float32')
-
-# Scale pixel intensities of the images from [0, 255] down to [0, 1]
-train_data /= 255.0
-test_data /= 255.0
-
-#building a linear stack of layers (with add() function) with the sequetial model
-model = Sequential()
-model.add(Flatten(input_shape=(28, 28)))  #input layer
-
-model.add(Dense(512))                     # hidden layer
-model.add(Activation('relu')) 
-
-model.add(Dense(512))                     # hidden layer
-model.add(Activation('relu'))  
-
-model.add(Dense(10))                      # output layer
-model.add(Activation('softmax'))  
-
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-history = model.fit(train_data, train_label, epochs=10, validation_data=(test_data, test_label))
-
-#saving the model
-save_dir = "results"
-model_name = "trained_keras_mnist"
-model_path = os.path.join(save_dir, model_name)
-model.save(model_path)
-print('saved trained model at %s ', model_path)
-
-#plotting the metrics
-fig = plt.figure()
-plt.subplot(2,1,1)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='lower right')
-
-plt.subplot(2,1,2)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper right')  
-
-plt.tight_layout()
-plt.savefig('metrics')
-
-
-loss_and_metrics = model.evaluate(test_data, test_label, verbose=2)
-
-print("test loss", loss_and_metrics[0])
-print("test accuracy", loss_and_metrics[1])
-
-labels = [i for i in range(10)]
-
-prediction = model.predict(test_data)
-row = 5
-colums = 6
-labels = [i for i in range(10)]
-for i in range(30):  
-    plt.subplot(colums, row, i + 1)
-    index = randint(0, len(test_data))
-    plt.imshow(test_data[index], cmap='gray_r')
-    plt.title(f"pre={labels[np.argmax(prediction[index])]} real={labels[test_label[index]]}")
-    plt.axis('off')
-plt.show()
-#fig.savefig('demo.png', bbox_inches='tight')
+train_mnist()
--- src/backend/predict_interface.py
+++ src/backend/predict_interface.py
@@ -1,22 +1,29 @@
-import cv2 as cv, os, math
-from task_types import Task_type
+import os
+import math
+import cv2 as cv
 from tensorflow import keras 
 import numpy as np
 import pandas as pd
 from PIL import Image
 import pytesseract as pyt
+from task_types import Task_type
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+digits_mapp = list(range(10))
+emnist_letters_mapp = list(range(97, 122+1))
 mapp = pd.read_csv("docs/emnist-balanced-mapping.txt", delimiter= ',', header=None, squeeze=True)
-letters_mapp = [l for l in range(97, 122+1)]
-digits_model = keras.models.load_model("results/trained_mnist_dnn_model")
+digits_model = keras.models.load_model("results/trained_emnist_digits")
 digits_model_mnist = keras.models.load_model("results/trained_keras_mnist")
-byMerge_model = keras.models.load_model("results/trained_emnist_dnn")
-balanced_model = keras.models.load_model("results/trained_emnist")
+by_merge_model = keras.models.load_model("results/trained_emnist_bymerge")
+balanced_model = keras.models.load_model("results/trained_emnist_balanced")
 letters_model = keras.models.load_model("results/trained_emnist_letters")
-err_msg = "type not valid! TYPE: [2] for numbers, [3] for text with numbers, [4] for text without numbers"
+ERR_MSG = "type not valid! TYPE: [2] for numbers, [3] for text with numbers, [4] for text without numbers"
 
+'''
+    reshape the image array size to 28x28 by filling its edges with zeros, according
+    to the number of times zeros should be added before the first element and 
+    after the last element of each axis of the image array.
+'''
 def enlarge_image(image):
     rows, cols = image.shape
     if rows > cols:
@@ -29,67 +36,100 @@ def enlarge_image(image):
         cols = 20
         rows = int(round(rows * factor))
         image = cv.resize(image, (cols, rows), interpolation=cv.INTER_CUBIC)
-    colsPadding = (int(math.ceil((28 - cols) / 2.0)), int(math.floor((28 - cols) / 2.0)))
-    rowsPadding = (int(math.ceil((28 - rows) / 2.0)), int(math.floor((28 - rows) / 2.0)))
-    image = np.lib.pad(image, (rowsPadding, colsPadding), 'constant')
+    cols_padding = (int(math.ceil((28 - cols) / 2.0)), int(math.floor((28 - cols) / 2.0)))
+    rows_padding = (int(math.ceil((28 - rows) / 2.0)), int(math.floor((28 - rows) / 2.0)))
+    image = np.lib.pad(image, (rows_padding, cols_padding), 'constant')
     padded = image.reshape(-1,28,28)
     padded = padded.astype(np.float32)
 
     return padded
 
+'''
+    Reshapes the image before using a model based on a 2D convolutional neural network.
+    Changes the type of reshaped image arrays to allow normalisation (downscaling of 
+    pixels to the interval [0 1]).
+    Normalisation is very important to achieve higher accuracy in prediction.
+'''
+def reshape_image_for_cnn_prediction(image):
+    image = image.reshape(-1, 28, 28, 1)
+    image = image.astype(np.float32)
+    image = image/255.0 
+    
+    return image
+
+'''
+    Predicts the image with each of the model corresponding to the task type 
+    specified as parameter.
+    Compares the prediction rate of each of these models with the other.
+    Returns the result of the model with the highest prediction rate.
+    The index of the mapp is equivalent to the index of the prediction array 
+    having the highest prediction rate.  
+    By the type : TEXT_NO_NUMBERS there is just one model available, therefore 
+    no comparison will be performed.  
+    When no type selected a valueerror is thrown.
+'''
 def ocr_pre(image, type):
-    if  (type == Task_type.NUMBER) :  
-        list_emnist_digits = digits_model.predict(image)
+    if(type == Task_type.NUMBER):  
+
         list_mnist = digits_model_mnist.predict(image)
+        image = reshape_image_for_cnn_prediction(image)
+        list_emnist_digits = digits_model.predict(image)
+
         pre_rate_emnist = max(list_emnist_digits)
         pre_rate_mnist = max(list_mnist)
-        labels = [i for i in range(10)]
-        if (pre_rate_emnist > pre_rate_mnist).any():
-            result = labels[np.argmax(list_emnist_digits)]
-        else:
-            result = labels[np.argmax(list_mnist)]
+       
+        if(pre_rate_emnist > pre_rate_mnist).any():
+            result = digits_mapp[np.argmax(list_emnist_digits)]
 
-    elif (type == Task_type.TEXT_NO_NUMBERS) :
-        prediction_byMerge_model = byMerge_model.predict(image)
-        prediction_balanced_model = balanced_model.predict(image)
-        prediction_letters_model = letters_model.predict(image)
-        index1 = np.argmax(prediction_byMerge_model)
-        index2 = np.argmax(prediction_balanced_model)
-        index3 = np.argmax(prediction_letters_model)
-        pre_rate_byMerge = max(prediction_byMerge_model)
-        pre_rate_balanced = max(prediction_balanced_model)
-        pre_rate_letters = max(prediction_letters_model)
-        index = index3-1
-         
-        if (max(pre_rate_byMerge) > max(pre_rate_balanced)).any():
-            if (max(pre_rate_byMerge) > max(pre_rate_letters)).any():
-                result = chr(mapp[index1])
-            else:
-                result = chr(letters_mapp[index])
         else:
-            if (max(pre_rate_balanced) > max(pre_rate_letters)).any():
-                result = chr(mapp[index2])
-            else:
-                result = chr(letters_mapp[index])
-    
-    elif (type == Task_type.TEXT):
+            result = digits_mapp[np.argmax(list_mnist)]
+
+    elif(type == Task_type.TEXT):
+
+        image = reshape_image_for_cnn_prediction(image)
+        prediction_by_merge_model = by_merge_model.predict(image)
         prediction_balanced_model = balanced_model.predict(image)
-        index3 = np.argmax(prediction_balanced_model)
-        result = chr(mapp[index3])
+
+        index1 = np.argmax(prediction_by_merge_model)
+        index2 = np.argmax(prediction_balanced_model)
+
+        pre_rate_by_merge = np.amax(prediction_by_merge_model)
+        pre_rate_balanced = np.amax(prediction_balanced_model)
+
+
+        if(pre_rate_by_merge > pre_rate_balanced):
+            result = chr(mapp[index1])
+
+        else:
+            result = chr(mapp[index2])
+
+    elif(type == Task_type.TEXT_NO_NUMBERS):                       
+        prediction_letters_model = letters_model.predict(image)
+        index3 = np.argmax(prediction_letters_model)
+        index = index3-1
+        result = chr(emnist_letters_mapp[index])
+            
     else:
-        raise ValueError(err_msg)
+        raise ValueError(ERR_MSG)
 
     return result
 
+'''
+    Uses traineddata files built using tesseract-ocr engine to predict images as block of text.
+    Predicts only normal letters and diacritical letters (lower- and uppercase).
+    Converts the output to string and returns it.
+    Use a special function "os.linesep,join()" to remove empty lines from the output. 
+    When no type selected a Valueerror is thrown.
+'''
 def pre_ocr_with_tesseract(image, type):
 
     if(type == Task_type.TEXT_NO_NUMBERS):
         img = Image.fromarray(image)
-        config = r'-l deu_ocr+deu --oem 2 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8'
+        config = r'-l ger_ocr --oem 2 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 6'
         result = pyt.image_to_string(img, config=config)
         result = os.linesep.join([s for s in result.splitlines() if s])
 
     else:
-        raise ValueError(err_msg)
-       
+        raise ValueError(ERR_MSG)
+
     return result
